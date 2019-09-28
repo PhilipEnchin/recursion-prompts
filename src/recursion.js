de2bace0789dca46eb052c1abd28825d912e938c
Philip Enchin <Philip@Ench.in>
2019-09-27 22:22:19 -0400

    Implement nthFibo

--- src/recursion.js
+++ src/recursion.js
@@ -237,6 +237,8 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+  if (n < 0) {return null;}
+  return n <= 1 ? n : nthFibo(n - 1) + nthFibo(n - 2);
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.

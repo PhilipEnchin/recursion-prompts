36a11e9f440f6eaddee96b4abc4443871d273f3f
Philip Enchin <Philip@Ench.in>
2019-09-27 22:19:57 -0400

    Implement fibonacci()

--- src/recursion.js
+++ src/recursion.js
@@ -224,6 +224,11 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+  if (n <= 0) {return null;}
+  if (n === 1) {return [0, 1];}
+  var fibArr = fibonacci(n - 1);
+  fibArr.push(fibArr[n - 1] + fibArr[n - 2]);
+  return fibArr;
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
